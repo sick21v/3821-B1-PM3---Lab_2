0c63459107f9691cbff9dad68ecb5fb50229f194
sick21v <90762376+sick21v@users.noreply.github.com>
2021-11-15 22:19:18 +0300

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,10 @@
+#ifndef _HEADER_H
+#define _HEADER_H
+
+void BubbleSort(int n, double a[]);
+
+void InsertionSort(int n, double a[]);
+
+void qs(double* items, int left, int right);
+
+#endif // !_HEADER_H
